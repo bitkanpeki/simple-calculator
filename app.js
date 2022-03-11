d2db09462e0db9047e00a3eacfc4808f6fb554fa
Jonathan Boulay <jonathanboulay@outlook.com>
2022-03-11 09:13:57 +0900

    Complete operators functionality

--- app.js
+++ app.js
@@ -1,26 +1,83 @@
+const numbers = document.querySelectorAll('.number')
+const display = document.querySelector('.display')
+const addButton = document.querySelector('.add')
+const substractButton = document.querySelector('.substract')
+const multiplyButton = document.querySelector('.multiply')
+const divideButton = document.querySelector('.divide')
+const equalButton = document.querySelector('.equal')
+
+let currentValue = 0
+let operator
+let total = 0
+let firstValue = 0
+
+const showDisplay = (value) => {
+  display.textContent = value
+}
+
+showDisplay(currentValue)
+
+const resetCurrentValue = () => (currentValue = '')
+
+const storeValue = (e) => {
+  if (total === 0) resetCurrentValue()
+  currentValue += e.target.textContent
+  currentValue = Number(currentValue)
+  showDisplay(currentValue)
+}
+
 const operations = {
-  add: (a, b) => {
-    console.log(a + b)
+  add: () => {
+    total = firstValue + currentValue
+    showDisplay(total)
+    currentValue = total
   },
 
-  substract: (a, b) => {
-    console.log(a - b)
+  substract: () => {
+    total = firstValue - currentValue
+    showDisplay(total)
+    currentValue = total
   },
 
-  multiply: (a, b) => {
-    console.log(a * b)
+  multiply: () => {
+    total = firstValue * currentValue
+    showDisplay(total)
+    currentValue = total
   },
 
-  divide: (a, b) => {
-    console.log(a / b)
+  divide: () => {
+    total = firstValue / currentValue
+    showDisplay(total)
+    currentValue = total
   },
 }
 
-const operate = (operator, a, b) => {
-  operations[operator](a, b)
-}
+const operate = (operator) => operations[operator]()
 
-operate('add', 3, 5)
-operate('substract', 8, 2)
-operate('multiply', 3, 5)
-operate('divide', 10, 2)
+numbers.forEach((number) => number.addEventListener('click', storeValue))
+
+addButton.addEventListener('click', () => {
+  operator = 'add'
+  firstValue = currentValue
+  resetCurrentValue()
+})
+
+substractButton.addEventListener('click', () => {
+  operator = 'substract'
+  firstValue = currentValue
+  resetCurrentValue()
+})
+
+multiplyButton.addEventListener('click', () => {
+  operator = 'multiply'
+  firstValue = currentValue
+  resetCurrentValue()
+})
+
+divideButton.addEventListener('click', () => {
+  operator = 'divide'
+  firstValue = currentValue
+  resetCurrentValue()
+})
+
+equalButton.addEventListener('click', () => operate(operator))
